9ef6cae59b00636f2700f6c1da2dbc754213de60
Unknown-Pplayer <xwang2230@gmail.com>
2023-02-24 19:12:58 -0500

    First Commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelector('#addbtn').onclick = function() {
+    if (document.querySelector('#newtasks input').value.length == 0) {
+        alert("Please Enter a Task")
+    } else {
+        document.querySelector
+    }
+}
